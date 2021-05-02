d63e734112074d04bd88c2abffa147c3a802be5f
ian <ianmaguina@gmail.com>
2021-05-02 01:29:21 -0500

    navigation

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:petcare/src/pages/add_pet.dart';
 
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home temp'),
+    return Scaffold(
+      body: Text('Perfil usuario'),
+      bottomNavigationBar: Center(
+        child: IconButton(
+          icon: Icon(Icons.pets),
+          onPressed: () {
+            final route = MaterialPageRoute(builder: (context) => AddPetPage());
+
+            Navigator.push(context, route);
+          },
+        ),
+      ),
     );
   }
 }
