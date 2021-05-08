72933db0ea9d977b97fb6d03aeeccc7926db07c6
Giordanov2 <giordanitrox@gmail.com>
2021-05-07 21:39:34 -0500

    Mi primer commit

--- lib/src/models/service.dart
+++ lib/src/models/service.dart
@@ -1,1 +1,29 @@
+// To parse this JSON data, do
+//
+//     final service = serviceFromJson(jsonString);
 
+import 'dart:convert';
+
+Service serviceFromJson(String str) => Service.fromJson(json.decode(str));
+
+String serviceToJson(Service data) => json.encode(data.toJson());
+
+class Service {
+    Service({
+        this.id,
+        this.name,
+    });
+
+    int id;
+    String name;
+
+    factory Service.fromJson(Map<String, dynamic> json) => Service(
+        id: json["id"],
+        name: json["name"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+    };
+}
