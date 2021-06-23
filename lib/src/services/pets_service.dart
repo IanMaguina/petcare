7a0033757f945f0f01397353f348556242340c87
Jorge Vital <49732779+JorgeDanielVital@users.noreply.github.com>
2021-06-22 22:13:42 -0500

    final merge fix

--- lib/src/services/pets_service.dart
+++ lib/src/services/pets_service.dart
@@ -8,7 +8,7 @@ final token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjkiLCJuYmYiOjE2MjA0NTIxOTgsImV4cCI6MTYyMTA1Njk5OCwiaWF0IjoxNjIwNDUyMTk4fQ.G-jOetqvYbgACErTLsF3iimKNKeHSZooUXX0YH8LXFI";
 
 class PetsService with ChangeNotifier {
-  List<Pets> listadoPets = [];
+  List<Pet> listadoPets = [];
 
   PetsService() {
     this.getPetByCustomerId();
